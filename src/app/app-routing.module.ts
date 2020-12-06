482a59425933807194d0a5cba0b45f0a1ea2fa35
Levan Tskipuri <ltskipuri@emis.ge>
2020-12-06 21:03:05 +0400

    add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: '**', redirectTo: '/home', pathMatch: 'full' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { Injector, NgModule } from '@angular/core';
+import { Location } from '@angular/common';
 import { createCustomElement } from '@angular/elements';
+import { Router } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   entryComponents: [AppComponent],
 })
 export class AppModule {
-  constructor(private injector: Injector) {
+  constructor(
+    private injector: Injector,
+    private router: Router,
+    private location: Location
+  ) {
     const appElement = createCustomElement(AppComponent, {
       injector: this.injector,
     });
 
     customElements.define('app-element', appElement);
+
+    this.router.navigateByUrl(this.location.path(true));
+    this.location.subscribe((data) => {
+      this.router.navigateByUrl(data.url!);
+    });
   }
 
   ngDoBootstrap() {}
