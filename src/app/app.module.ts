cf2cc8e3c045dd0896eca119c04ac3a9d469af3f
Levan Tskipuri <ltskipuri@emis.ge>
2020-12-06 13:40:38 +0400

    dev mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const appElement = createCustomElement(AppComponent, {
+      injector: this.injector,
+    });
+
+    customElements.define('app-element', appElement);
+  }
+
+  ngDoBootstrap() {}
+}
